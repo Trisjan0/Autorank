0ae771d3facf95712bf7252220fba900e675284b
Trisjan0 <btrisjan@gmail.com>
2025-07-20 15:22:50 +0800

    feat: Added Spatie for roles and permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use HasRoles; // added for Spatie
 
 class User extends Authenticatable
 {
