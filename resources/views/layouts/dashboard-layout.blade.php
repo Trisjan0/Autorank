90765efe6f93cbb78ed35eb587dde0d1f786ecb1
Trisjan0 <btrisjan@gmail.com>
2025-08-16 14:24:54 +0800

    fix: js & css for ngrok sharing

--- resources/views/layouts/dashboard-layout.blade.php
+++ resources/views/layouts/dashboard-layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="user-id" content="{{ auth()->id() }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> <!-- for ngrok -->
     <title>@yield('title', 'Autorank')</title>
 
     <!-- Google Font Links -->
